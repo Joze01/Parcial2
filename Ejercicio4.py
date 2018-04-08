b3333122d2ff73ff55221df8a9e4adb1672919c5
Java <>
2018-04-08 10:36:36 -0600

    Correcion ejercicio 4

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -12,6 +12,16 @@ import numpy as np
 datos1 = pd.read_csv('Laboratorio2.csv', encoding='latin1', header=0)
 df = pd.DataFrame(datos1)
 
+df['Fecha_Alta']=pd.to_datetime(df['Fecha_Alta'])
+start_date=pd.to_datetime("31-12-06")
+end_date=pd.to_datetime("01-01-08")
+
+
+mask = (df['Fecha_Alta'] > start_date) & (df['Fecha_Alta'] < end_date)
+df = df.loc[mask]
+
+print(df)
+
 
 frameLocal=df['Tipo'].groupby(df['Tipo']).count().reset_index(name='Cantidad').sort_values(['Cantidad'], ascending=False)
 
@@ -29,4 +39,4 @@ ax1.pie(sizes,  labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.show()
+#plt.show()
