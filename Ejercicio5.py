635edb66dd9ee5540762617a27b2fe9cde93eece
Java <>
2018-04-08 20:12:34 -0600

    Corrección
    Corrección ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -11,15 +11,15 @@ datos1 = pd.read_csv('Laboratorio2.csv', encoding='latin1', header=0)
 df = pd.DataFrame(datos1)
 
 #SET SHOW ALL DF
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 
-#mask=((df['Tipo']=='Parqueo') | (df['Tipo']=='Oficina'))
-#df = df.loc[mask]
 
-vendedores=df['Vendedor'].groupby(df['Vendedor']).count().reset_index(name='Ventas_Realizadas').sort_values(['Ventas_Realizadas'], ascending=False)
+
+#print(df)
+vendedores=df.groupby(['Tipo','Vendedor'])['Precio_Venta'].count().reset_index(name='Ventas_Realizadas').sort_values(['Tipo','Ventas_Realizadas'], ascending=False)
 
 
 print("El vendedor con mas ventas realizadas es: ")
 
-print(vendedores.head(1))
+print(vendedores.groupby('Tipo').nth(0))
